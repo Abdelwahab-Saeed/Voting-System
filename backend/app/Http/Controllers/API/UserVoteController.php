4c10c8825217ff5b52ccb3d591e2f0b268b6c86f
Abdelwahab Saeed Younes <abdelwahabsaeed415@gmail.com>
2026-03-14 07:54:39 +0200

    create UserVotes model and controller, migrations and make the relations with it and the user model

---
+++ backend/app/Http/Controllers/API/UserVoteController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\UserVote;
+use Illuminate\Http\Request;
+
+class UserVoteController extends Controller
+{
+    
+}

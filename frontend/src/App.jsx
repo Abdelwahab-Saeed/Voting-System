83419722b893472f8ca8c41f36ba048599737438
Abdelwahab Saeed Younes <abdelwahabsaeed415@gmail.com>
2026-03-17 01:33:40 +0200

    feat: Implement frontend voting board login with API integration.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,17 +2,35 @@ import { Routes, Route } from 'react-router-dom'
 import { Header } from './layout/Header'
 import { Footer } from './layout/Footer'
 import { Login } from './pages/Login'
+import { Register } from './pages/Register'
+import { ProtectedRoute } from './components/ProtectedRoute'
+import { GuestRoute } from './components/GuestRoute'
+import { VotingBoard } from './pages/VotingBoard'
+import { PendingApproval } from './pages/PendingApproval'
 
 export const App = () => {
   return (
-    <>
-      <Header />  
+    <div className="d-flex flex-column min-vh-100">
+      <Header />
       <Routes>
-        
-        <Route path="/" element={<div>Home</div>} />
-        <Route path="/login" element={<Login />} />
+        <Route path="/" element={
+          <ProtectedRoute>
+            <VotingBoard />
+          </ProtectedRoute>
+        } />
+        <Route path="/login" element={
+          <GuestRoute>
+            <Login />
+          </GuestRoute>
+        } />
+        <Route path="/register" element={
+          <GuestRoute>
+            <Register />
+          </GuestRoute>
+        } />
+        <Route path="/pending-approval" element={<PendingApproval />} />
       </Routes>
-      <Footer/>
-    </>
+      <Footer />
+    </div>
   )
 }

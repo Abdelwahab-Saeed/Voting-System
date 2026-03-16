6fc0eee00af5c0b13a638fc93e4a58a9da977857
Abdelwahab Saeed Younes <abdelwahabsaeed415@gmail.com>
2026-03-17 01:32:48 +0200

    complete layout structure for footer and header

--- frontend/src/layout/Footer.jsx
+++ frontend/src/layout/Footer.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 export const Footer = () => {
   return (
-    <footer>
-        Footer
+    <footer className='bg-dark text-white text-center py-3 mt-auto'>
+      <p className='m-0'>Voting System © 2026</p>
     </footer>
   )
 }
